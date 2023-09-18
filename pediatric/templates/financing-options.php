e4e6bf0de881cf18febb6e0bf067764fb93dd89c
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2023-09-18 03:32:00 -0700

    updating classes

--- pediatric/templates/financing-options.php
+++ pediatric/templates/financing-options.php
@@ -42,7 +42,7 @@ partial('section.wrapper', [
       [
           'name' => 'section.pediatric.split-static',
           'parts' => [
-              'classes' => ['overlap-top'],
+              'classes' => ['overlap-top', 'big-img-left'],
               'heading' => get_post_meta(get_the_id(), 'financing_section_two_group_heading', true),
               'heading_classes' => ['line-height-1', 'h1', 'mb-20'],
               'copy' => apply_filters('the_content',get_post_meta(get_the_id(), 'financing_section_two_group_content', true)),
@@ -56,17 +56,10 @@ partial('section.wrapper', [
       [
         'name' => 'section.pediatric.bubbles',
         'parts' => [
-            'classes' => ['white', 'bottom', 'var-5-sparse']
+            'classes' => ['blue', 'bottom', 'var-1']
         ]
-    ]
+      ]
   ]
-]);
-
-partial('section.copy.overlapping-bubbles', [
-    'classes' => [$brand->post_name],
-    'bubbles' => $overlapping_bubbles,
-    'right_heading' =>  '',
-    'right_copy' =>  '',
 ]);
 
 $icons = [
