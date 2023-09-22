a004e8d2fc9689feabc400e5777ec462562caed6
phamiter253 <34218345+phamiter253@users.noreply.github.com>
2023-09-22 11:09:26 -0700

    updating svg php file

--- pediatric/partials/widget/icons-hover/insurance.php
+++ pediatric/partials/widget/icons-hover/insurance.php
@@ -1,6 +1,31 @@
 <div class="icons-hover-container">
     <div class="widget icons-hover icon-h-insurance">
-        <div class="path1"></div>
-        <div class="path2"></div>
+    <div class="icon-insur active">
+							<svg xmlns="http://www.w3.org/2000/svg" width="120.075" height="107" viewBox="0 0 120.075 107" style="height: inherit;">
+				<g id="sim_insurance" data-name="Group 9" transform="translate(-211 -160)">
+				  <rect id="Rectangle_1" data-name="Rectangle 1" width="78" height="107" rx="5" transform="translate(211 160)" fill="#558bdb"/>
+				  <path id="Path_7" data-name="Path 7" d="M14.617,28.336H60.986" transform="translate(207.199 151.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <path id="Path_8" data-name="Path 8" d="M14.617,28.336H50.175" transform="translate(207.199 163.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <path id="Path_9" data-name="Path 9" d="M14.617,28.336H60.986" transform="translate(207.199 175.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <path id="Path_10" data-name="Path 10" d="M14.617,28.336H50.175" transform="translate(207.199 187.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <path id="Path_11" data-name="Path 11" d="M14.617,28.336H60.986" transform="translate(207.199 199.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <path id="Path_12" data-name="Path 12" d="M14.617,28.336H50.175" transform="translate(207.199 211.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <path id="Path_13" data-name="Path 13" d="M14.617,28.336H60.986" transform="translate(207.199 223.062)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="3"/>
+				  <g id="Group_5" data-name="Group 5">
+					<g id="Group_2" data-name="Group 2" transform="translate(132 -12074.593)">
+					  <g id="Group_1" data-name="Group 1" transform="translate(145 12253.99)">
+						<path id="incurance-icon-right" data-name="Path 1" d="M172.038,12322.2c-27.247-10.02-26.987-43.183-26.987-43.183l-.051.022v-15.617h6.69c14.783,0,20.278-9.428,20.278-9.428h.14s5.5,9.428,20.278,9.428h6.689v15.617l-.048-.022s.265,33.163-26.99,43.183" transform="translate(-145 -12253.99)" fill="#a7c5ec"/>
+					  </g>
+					</g>
+					<g id="Group_4" data-name="Group 4" transform="translate(-253 -12314.999)">
+					  <g id="Group_3" data-name="Group 3">
+						<path id="Path_2" data-name="Path 2" d="M555.953,12538a2,2,0,0,1-1.332-.509l-8.954-8a2,2,0,0,1,2.666-2.982l7.262,6.489,9.759-14.135a2,2,0,0,1,3.292,2.273l-11.047,16a2,2,0,0,1-1.409.85A2.143,2.143,0,0,1,555.953,12538Z" fill="#fff"/>
+					  </g>
+					</g>
+				  </g>
+				</g>
+			</svg>			
+
+			</div>
     </div>
 </div>
