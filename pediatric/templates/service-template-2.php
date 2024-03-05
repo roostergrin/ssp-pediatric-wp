8d1b161152307f63a62702965e80db91aaf0451d
jason murphy <jmurphy.1196@gmail.com>
2024-03-05 11:32:30 -0800

    fixes color for green bubble in service template 2

--- pediatric/templates/service-template-2.php
+++ pediatric/templates/service-template-2.php
@@ -189,7 +189,7 @@ partial('section.wrapper', [
 		[
 			'name' => 'section.slider.slider',
 			'parts' => [
-				'classes' => [(sanitize_title($brand->palette) == 'smiles-in-motion' ? 'bg-green' : 'bg-blue')],
+				'classes' => ['three-icons', (sanitize_title($brand->palette) == 'smiles-in-motion' ? 'bg-green' : 'bg-blue')],
 				'htag' => 'h3',
 				'heading_classes' => ['white'],
 				'heading' => $sec_8_heading,
