d78289bc72e446b5c628d2647ba7cb6cbdd65d94
jason murphy <jmurphy.1196@gmail.com>
2024-09-10 14:41:06 -0700

    requests testing pages

--- pediatric/functions.php
+++ pediatric/functions.php
@@ -1045,7 +1045,7 @@ add_action('wp', function($template) {
 				$p = virtual_redirect_get_post( $post_type, $post_parent, $post_name );
 
 				// don't know if this is ever accessed???
-				print_stmt($p, 1);
+				print_stmt($post_name, 1);
 
 				if(!empty($p) && !empty($p->ID)) {
 					do_virtual_page(get_post($p->ID));
