e14f9b175436797051280216a5be60bdbe9456c3
jason murphy <jmurphy.1196@gmail.com>
2024-12-10 21:16:35 -0800

    req

--- pediatric/partials/section/pediatric/two-icons-with-image.php
+++ pediatric/partials/section/pediatric/two-icons-with-image.php
@@ -29,7 +29,9 @@ wp_enqueue_script('two-icons-with-image-carousel');
                         <div class="slides-container two-icons-with-image-carousel">
                             <? foreach ($slides as $slide): ?>
                                 <div class="slide">
-                                    <div class="icon"><i class="widget static icon-<?= $slide['icon']; ?>"></i></div>
+                                    <?php if (!$no_icon): ?>
+                                        <div class="icon"><i class="widget static icon-<?= $slide['icon']; ?>"></i></div>
+                                    <?php endif; ?>
                                     <div class="heading"><?= $slide['heading']; ?></div>
                                     <div class="copy"><?= $slide['copy']; ?></div>
                                 </div>
