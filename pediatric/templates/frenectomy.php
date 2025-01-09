0ceda9ba7acd8e8823b5f967cffeda0638fa6348
jason murphy <jmurphy.1196@gmail.com>
2025-01-09 15:57:47 -0800

    requests

--- pediatric/templates/frenectomy.php
+++ pediatric/templates/frenectomy.php
@@ -180,7 +180,7 @@ partial('section.wrapper', [
         [
             'name' => 'section.copy.signs_and_symptoms',
             'parts' => [
-            'classes' => [],
+            'classes' => ['bg-green, text-white'],
             ]
         ],
         [
