e5b77b950772bf5fce0a797c709fd27d134bf90e
jason murphy <jmurphy.1196@gmail.com>
2025-01-09 16:45:18 -0800

    requests

--- pediatric/partials/section/copy/signs_and_symptoms.php
+++ pediatric/partials/section/copy/signs_and_symptoms.php
@@ -6,9 +6,45 @@
                 <div class="signs_and_symptoms__cards">
                     <div class="signs_and_symptoms__card">
                         <h2 class="signs_and_symptoms__header">Baby signs and symptoms</h2>
+                        <div class="signs_and_symptoms__list-container">
+                            <ul class="signs_and_symptoms__list">
+                                <li class="signs_and_symptoms__item">Shallow latch</li>
+                                <li class="signs_and_symptoms__item">Lip curls under while feeding</li>
+                                <li class="signs_and_symptoms__item">Loses suction</li>
+                                <li class="signs_and_symptoms__item">Prolonged/incomplete feedings</li>
+                                <li class="signs_and_symptoms__item">Falls asleep in the middle of feeding</li>
+                                <li class="signs_and_symptoms__item">Gagging or coughing while feeding</li>
+                                <li class="signs_and_symptoms__item">Chewing nipples while feeding</li>
+                            </ul>
+                            <ul class="signs_and_symptoms__list">
+                                <li class="signs_and_symptoms__item">Clicking/smacking noises while feeding</li>
+                                <li class="signs_and_symptoms__item">Milk leakage out of their mouth</li>
+                                <li class="signs_and_symptoms__item">Poor or slow weight gain</li>
+                                <li class="signs_and_symptoms__item">Frequently gassiness and fussiness</li>
+                                <li class="signs_and_symptoms__item">Inability to hold pacifier</li>
+                                <li class="signs_and_symptoms__item">Lip blisters</li>
+                                <li class="signs_and_symptoms__item">High arched and palate</li>
+                            </ul>
+                        </div>
                     </div>
                     <div class="signs_and_symptoms__card">
                         <h2 class="signs_and_symptoms__header">Mother's signs and symptoms</h2>
+                        <div class="signs_and_symptoms__list-container">
+                            <ul class="signs_and_symptoms__list">
+                                <li class="signs_and_symptoms__item">Painful nursing</li>
+                                <li class="signs_and_symptoms__item">Nipples are creased, flattened, and/or blanched</li>
+                                <li class="signs_and_symptoms__item">Nipples are cracked, blistered, and/or bleeding</li>
+                                <li class="signs_and_symptoms__item">Poor or incomplete breast drainage</li>
+                            </ul>
+                                <ul class="signs_and_symptoms__list">
+                                <li class="signs_and_symptoms__item">Plugged ducts or mastitis</li>
+                                <li class="signs_and_symptoms__item">Inability to nurse without a nipple shield</li>
+                                <li class="signs_and_symptoms__item">Decreased milk supply</li>
+                            </ul>
+
+                        </div>
+
+
                     </div>
                     <div class="signs_and_symptoms__card"></div>
                 </div>
