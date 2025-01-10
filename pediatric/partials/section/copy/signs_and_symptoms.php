16b0fc9403def39ab43a8915d3d206cad9afc1a1
jason murphy <jmurphy.1196@gmail.com>
2025-01-09 16:12:12 -0800

    requests

--- pediatric/partials/section/copy/signs_and_symptoms.php
+++ pediatric/partials/section/copy/signs_and_symptoms.php
@@ -1,32 +1,20 @@
 <section class="signs_and_symptoms <?= $classes ? ' ' . implode(' ', $classes) : ''; ?>">
-    <div class="signs_and_symptoms_content">
-        <div class="signs_and_symptoms__cards">
-            <div class="signs_and_symptoms__card">
-                <h2 class="signs_and_symptoms__header">Baby signs and symptoms</h2>
-            </div>
-            <div class="signs_and_symptoms__card">
-                <h2 class="signs_and_symptoms__header">Mother's signs and symptoms</h2>
-            </div>
-            <div class="signs_and_symptoms__card"></div>
-        </div>
-        <div class="signs_and_symptoms__images"></div>
+    <div class="content">
+
+        <div class="inner-content">
+            <div class="signs_and_symptoms_content">
+                <div class="signs_and_symptoms__cards">
+                    <div class="signs_and_symptoms__card">
+                        <h2 class="signs_and_symptoms__header">Baby signs and symptoms</h2>
+                    </div>
+                    <div class="signs_and_symptoms__card">
+                        <h2 class="signs_and_symptoms__header">Mother's signs and symptoms</h2>
+                    </div>
+                    <div class="signs_and_symptoms__card"></div>
+                </div>
+                <div class="signs_and_symptoms__images"></div>
     </div>
-<style>
-    .text-white {
-        color: white;
-    }
-    .signs_and_sypmtoms_content {
-        display: flex;
-    }
-    .signs_and_sypmtoms_cards {
-        display: flex;
-        flex-direction: column;
-        gap: 3rem;
-    }
-    .signs_and_sypmtoms_card {
-        background: white;
-        color: black;
-        border-radius: 5px;
-    }
-</style>
+
+        </div>
+    </div>
 </section>
